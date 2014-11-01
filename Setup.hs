c4b76c7fb4f0ee35fdb7bc9771fa720c81e3bd04
Edward Kmett <ekmett@gmail.com>
2014-11-01 16:18:57 -0400

    merged gl-gen into Setup.hs and removed build target

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,17 @@
 import Distribution.Simple
--- import Gen.Registry
--- import Gen.Parser
--- import Gen.Module
--- import Gen.Generator
-main = defaultMain
+import Gen.Generator
+import Gen.Parser
+import Gen.Registry
+
+generateAPI :: IO ()
+generateAPI = do
+  registry <- parseFile "gl.xml"
+  -- TODO: check the date on gl.xml, Gen.*, Setup.hs against dist/build/autogen/Graphics?
+  generateSource (deshenaniganize registry)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { buildHook = \pkg lbi hooks flags -> do
+    generateAPI
+    buildHook simpleUserHooks pkg lbi hooks flags
+  }
