7313f4af8e31dca73434f11864bf17dc05a734fc
Edward Kmett <ekmett@gmail.com>
2014-11-01 17:30:26 -0400

    silence the autogen warning on build by generating the dependency during sdist

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription
 import System.FilePath
 import Generator (generateSource)
 import Parser (parseFile)
@@ -18,5 +19,9 @@ main = defaultMainWithHooks simpleUserHooks
   { buildHook = \p l h f -> generateAPI l >> buildHook simpleUserHooks p l h f
   , sDistHook = \p ml h f -> case ml of
      Nothing -> fail "No local buildinfo available. configure first"
-     Just l -> generateAPI l >> sDistHook simpleUserHooks p ml h f
+     Just l -> do
+       let editlib lib = lib { libBuildInfo = editBuildInfo (libBuildInfo lib) }
+           editBuildInfo bi = bi { hsSourceDirs = (buildDir l </> "autogen") : hsSourceDirs bi }
+           p' = p { library = fmap editlib (library p) }
+       generateAPI l >> sDistHook simpleUserHooks p' ml h f
   }
