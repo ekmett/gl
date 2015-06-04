9890558fc61537efab129cd83d7fc712056c5ab2
Edward Kmett <ekmett@gmail.com>
2015-06-04 13:57:41 +0200

    Supply our own splitOn to avoid the dependency on `split` from the Platform. This may help with non-platform installs

--- Utils.hs
+++ Utils.hs
@@ -12,11 +12,19 @@
 module Utils (splitOn, strip, replace) where
 
 import Data.Char (isSpace)
-import Data.List (intercalate)
-import Data.List.Split (splitOn)
+import Data.List (intercalate, isPrefixOf)
 
 strip :: String -> String
 strip = dropWhile isSpace . reverse . dropWhile isSpace . reverse
 
 replace :: Eq a => [a] -> [a] -> [a] -> [a]
 replace a b = intercalate b . splitOn a
+
+splitOn :: Eq a => [a] -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn xs ys0 = go [] ys0 where
+  go acc ys
+    | isPrefixOf xs ys = reverse acc : go [] (drop (length xs) ys)
+    | otherwise = case ys of
+      z:zs -> go (z:acc) zs
+      [] -> [reverse acc]
